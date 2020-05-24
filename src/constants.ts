3ad43e2b36c81cf3ae7a06ca02d62e2f11de6774
Serge Vartanov <vartanov.s@gmail.com>
2020-05-24 14:46:16 -0700

    modeling prep income statement constants and types

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,81 @@
+import type { ModelingPrepIncomeStatementKey, IncomeStatementKey } from './types';
+
+export const FETCH_SLEEP_TIMEOUT_MS = 50;
 export const MODELING_PREP_STOCK_CHUNK_SIZE = 3;
 export const POSTGRES_SLEEP_TIMEOUT_MS = 50;
-export const SLEEP_TIMEOUT_MS = 20;
 
 // Default control flags
-export const UPDATE_FINANCIALS = true;
+export const UPDATE_INCOME_STATEMENTS = false;
 export const UPDATE_PRICES = true;
 export const UPDATE_STOCK_LIST = false;
+
+// Modeling Prep API <> Income Statement
+export const modelingPrepKeysToIncomeStatementDict: Record<ModelingPrepIncomeStatementKey, IncomeStatementKey> = {
+  'date': 'date',
+  'Revenue': 'revenue',
+  'Revenue Growth': 'revenue_growth',
+  'Cost of Revenue': 'cost_of_revenue',
+  'Gross Profit': 'gross_profit',
+  'R&D Expenses': 'rd_expense',
+  'SG&A Expense': 'sga_expense',
+  'Operating Expenses': 'operating_expense',
+  'Operating Income': 'operating_income',
+  'Interest Expense': 'interest_expense',
+  'Earnings before Tax': 'ebt',
+  'Income Tax Expense': 'income_tax_expense',
+  'Net Income - Non-Controlling int': 'net_income_non_controlling',
+  'Net Income - Discontinued ops': 'net_income_discontinued',
+  'Net Income': 'net_income',
+  'Preferred Dividends': 'preferred_dividends',
+  'Net Income Com': 'net_income_com',
+  'EPS': 'eps',
+  'EPS Diluted': 'eps_diluted',
+  'Weighted Average Shs Out': 'shares_outstanding',
+  'Weighted Average Shs Out (Dil)': 'shares_outstanding_diluted',
+  'Dividend per Share': 'dividend_per_share',
+  'Gross Margin': 'gross_margin',
+  'EBITDA Margin': 'ebitda_margin',
+  'EBIT Margin': 'ebit_margin',
+  'Profit Margin': 'profit_margin',
+  'Free Cash Flow margin': 'free_cash_flow_margin',
+  'EBITDA': 'ebitda',
+  'EBIT': 'ebit',
+  'Consolidated Income': 'consolidated_income',
+  'Earnings Before Tax Margin': 'ebt_margin',
+  'Net Profit Margin': 'net_profit_margin',
+};
+
+export const incomeStatementKeysToModelingPrepDict: Record<Exclude<IncomeStatementKey, 'symbol'>, ModelingPrepIncomeStatementKey> = {
+  date: 'date',
+  revenue: 'Revenue',
+  revenue_growth: 'Revenue Growth',
+  cost_of_revenue: 'Cost of Revenue',
+  gross_profit: 'Gross Profit',
+  rd_expense: 'R&D Expenses',
+  sga_expense: 'SG&A Expense',
+  operating_expense: 'Operating Expenses',
+  operating_income: 'Operating Income',
+  interest_expense: 'Interest Expense',
+  ebt: 'Earnings before Tax',
+  income_tax_expense: 'Income Tax Expense',
+  net_income_non_controlling: 'Net Income - Non-Controlling int',
+  net_income_discontinued: 'Net Income - Discontinued ops',
+  net_income: 'Net Income',
+  preferred_dividends: 'Preferred Dividends',
+  net_income_com: 'Net Income Com',
+  eps: 'EPS',
+  eps_diluted: 'EPS Diluted',
+  shares_outstanding: 'Weighted Average Shs Out',
+  shares_outstanding_diluted: 'Weighted Average Shs Out (Dil)',
+  dividend_per_share: 'Dividend per Share',
+  gross_margin: 'Gross Margin',
+  ebitda_margin: 'EBITDA Margin',
+  ebit_margin: 'EBIT Margin',
+  profit_margin: 'Profit Margin',
+  free_cash_flow_margin: 'Free Cash Flow margin',
+  ebitda: 'EBITDA',
+  ebit: 'EBIT',
+  consolidated_income: 'Consolidated Income',
+  ebt_margin: 'Earnings Before Tax Margin',
+  net_profit_margin: 'Net Profit Margin',
+};
--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,92 @@ export interface Stock {
   exchangeType: ExchangeType;
   symbol: string;
 }
+
+export interface StockPayload {
+  exchange_type: ExchangeType;
+  symbol: string;
+}
+
+export interface ModelingPrepFinancialsResponse {
+  financialStatementList: ModelingPrepIncomeStatements[];
+}
+
+// Modeling Prep Income Statement
+export type ModelingPrepIncomeStatementKey = keyof ModelingPrepIncomeStatement;
+export type IncomeStatementKey = keyof IncomeStatement;
+
+export interface IncomeStatement {
+  symbol: string;
+  date: string;
+  revenue: number;
+  revenue_growth: number;
+  cost_of_revenue: number;
+  gross_profit: number;
+  rd_expense: number;
+  sga_expense: number;
+  operating_expense: number;
+  operating_income: number;
+  interest_expense: number;
+  ebt: number;
+  income_tax_expense: number;
+  net_income_non_controlling: number;
+  net_income_discontinued: number;
+  net_income: number;
+  preferred_dividends: number;
+  net_income_com: number;
+  eps: number;
+  eps_diluted: number;
+  shares_outstanding: number;
+  shares_outstanding_diluted: number;
+  dividend_per_share: number;
+  gross_margin: number;
+  ebitda_margin: number;
+  ebit_margin: number;
+  profit_margin: number;
+  free_cash_flow_margin: number;
+  ebitda: number;
+  ebit: number;
+  consolidated_income: number;
+  ebt_margin: number;
+  net_profit_margin: number;
+}
+
+export interface ModelingPrepIncomeStatements {
+  'symbol': string;
+  'financials': ModelingPrepIncomeStatement[];
+}
+
+export interface ModelingPrepIncomeStatement {
+  'date': string;
+  'Revenue': string;
+  'Revenue Growth': string;
+  'Cost of Revenue': string;
+  'Gross Profit': string;
+  'R&D Expenses': string;
+  'SG&A Expense': string;
+  'Operating Expenses': string;
+  'Operating Income': string;
+  'Interest Expense': string;
+  'Earnings before Tax': string;
+  'Income Tax Expense': string;
+  'Net Income - Non-Controlling int': string;
+  'Net Income - Discontinued ops': string;
+  'Net Income': string;
+  'Preferred Dividends': string;
+  'Net Income Com': string;
+  'EPS': string;
+  'EPS Diluted': string;
+  'Weighted Average Shs Out': string;
+  'Weighted Average Shs Out (Dil)': string;
+  'Dividend per Share': string;
+  'Gross Margin': string;
+  'EBITDA Margin': string;
+  'EBIT Margin': string;
+  'Profit Margin': string;
+  'Free Cash Flow margin': string;
+  'EBITDA': string;
+  'EBIT': string;
+  'Consolidated Income': string;
+  'Earnings Before Tax Margin': string;
+  'Net Profit Margin': string;
+}
