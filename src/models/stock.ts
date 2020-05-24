167d8bc08de924847614c75f1ca41f41fee96436
Serge Vartanov <vartanov.s@gmail.com>
2020-05-24 14:53:15 -0700

    add seedFromCSV, readFromCSV and readAll methods to stock model

--- src/models/stock.ts
+++ src/models/stock.ts
@@ -1,18 +1,35 @@
 import csvParser from 'csv-parser';
 import dotenv from 'dotenv';
 import fs from 'fs';
+import { camelizeKeys } from 'humps';
 
-import type { Stock } from '../types';
+import postgresClient from '../clients/postgres';
+import { POSTGRES_SLEEP_TIMEOUT_MS, UPDATE_STOCK_LIST } from '../constants';
+import { sleep } from '../utils';
+
+import type { QueryResult } from 'pg';
+import type { Stock, StockPayload } from '../types';
 
 dotenv.config();
 
 const { PATH_TO_STOCK_LIST_CSV } = process.env;
 
 const stock = {
-  readAll() {
-    const stocks: Stock[] = [];
+  async readAll() {
+    if (UPDATE_STOCK_LIST) await this.seedFromCSV();
 
-    const addToStockList = (data: Stock) => {
+    const getAllQuery = 'SELECT * FROM stocks';
+
+    await postgresClient.connect();
+    const response = await postgresClient.query(getAllQuery) as QueryResult<StockPayload>;
+    postgresClient.end();
+    return camelizeKeys(response.rows) as Stock[];
+  },
+
+  readFromCSV() {
+    const stocks: StockPayload[] = [];
+
+    const addToStockList = (data: StockPayload) => {
       if (!data.symbol) {
         const errorMessage = 'Symbol missing for stock data';
         console.error(errorMessage, { data }); // eslint-disable-line
@@ -22,13 +39,36 @@ const stock = {
       stocks.push(data);
     };
 
-    return new Promise<Stock[]>((resolve, reject) => {
+    return new Promise<StockPayload[]>((resolve, reject) => {
       fs.createReadStream(PATH_TO_STOCK_LIST_CSV as string)
         .pipe(csvParser())
         .on('data', addToStockList)
         .on('error', (err) => reject(err))
         .on('end', () => resolve(stocks));
     });
+  },
+
+  async seedFromCSV() {
+    const stockData = await this.readFromCSV();
+
+    const seedQuery = `
+      INSERT INTO stocks (symbol, exchange_type)
+      VALUES ($1, $2)
+    `;
+
+    await postgresClient.connect();
+    for (let i = 0; i < stockData.length; i += 1) {
+      await sleep(POSTGRES_SLEEP_TIMEOUT_MS); // eslint-disable-line
+      const { symbol, exchange_type: exchangeType } = stockData[i];
+      try {
+        await postgresClient.query(seedQuery, [symbol, exchangeType]); // eslint-disable-line
+        console.log(`Seeded ${symbol} into "stocks" postgres table`); // eslint-disable-line
+      } catch (err) {
+        console.error(`Could not seed ${symbol} into "stocks" postgres table`, { err }); // eslint-disable-line
+      }
+    }
+
+    await postgresClient.end();
   },
 };
 
