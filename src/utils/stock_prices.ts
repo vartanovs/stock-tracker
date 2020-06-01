0ceee6dfa67779a46d516de8f36382f294b266f8
Serge Vartanov <vartanov.s@gmail.com>
2020-05-31 20:23:32 -0700

    add stock prices utils to format historic prices and get rounded price

---
+++ src/utils/stock_prices.ts
@@ -0,0 +1,28 @@
+import { QUARTERLY_STOCK_PRICE_DATES } from '../constants/dates';
+
+import type { Stock, StockPrice } from '../types';
+
+const [lastQtDate, twoQtDate, threeQtDate, lastYrDate, fiveQtDate] = QUARTERLY_STOCK_PRICE_DATES;
+const twoYrDate = QUARTERLY_STOCK_PRICE_DATES[7];
+const fiveYrDate = QUARTERLY_STOCK_PRICE_DATES[19];
+
+const getRoundedStockPrice = (stockPrice?: StockPrice) => {
+  if (!stockPrice) return 0;
+  const { low, high } = stockPrice;
+  return Math.floor((low + high) / 2);
+};
+
+export const formatHistoricPrices = (stocks: Stock[], historicPrices: StockPrice[]) => stocks
+  .map((stock) => {
+    const historicStockPrices = historicPrices.filter(({ symbol }) => symbol === stock.symbol);
+    return {
+      ...stock,
+      lastQtPrice: getRoundedStockPrice(historicStockPrices.find(({ date }) => (date as Date).toISOString().split('T')[0] === lastQtDate)),
+      twoQtPrice: getRoundedStockPrice(historicStockPrices.find(({ date }) => (date as Date).toISOString().split('T')[0] === twoQtDate)),
+      threeQtPrice: getRoundedStockPrice(historicStockPrices.find(({ date }) => (date as Date).toISOString().split('T')[0] === threeQtDate)),
+      lastYrPrice: getRoundedStockPrice(historicStockPrices.find(({ date }) => (date as Date).toISOString().split('T')[0] === lastYrDate)),
+      fiveQtPrice: getRoundedStockPrice(historicStockPrices.find(({ date }) => (date as Date).toISOString().split('T')[0] === fiveQtDate)),
+      twoYrPrice: getRoundedStockPrice(historicStockPrices.find(({ date }) => (date as Date).toISOString().split('T')[0] === twoYrDate)),
+      fiveYrPrice: getRoundedStockPrice(historicStockPrices.find(({ date }) => (date as Date).toISOString().split('T')[0] === fiveYrDate)),
+    };
+  });
