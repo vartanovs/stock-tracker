f1d2695fa03e787135bef31d75de6cda7aaebe64
Serge Vartanov <vartanov.s@gmail.com>
2020-05-31 20:27:29 -0700

    update main to make relevant get and save calls

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello World!');
+import incomeStatementModel from './models/income_statements';
+import recentFinancialsModel from './models/recent_financials';
+import stockModel from './models/stock';
+import stockPriceModel from './models/stock_price';
+import { formatIncomeStatementsToRecentFinancials } from './utils/recent_financials';
+
+const main = async () => {
+  const stocks = await stockModel.readAll();
+  const equitySymbols = stocks
+    .filter((stock) => stock.exchangeType === 'nyse' || stock.exchangeType === 'nasdaq')
+    .map(({ symbol }) => symbol);
+
+  const incomeStatements = await incomeStatementModel.readAll(equitySymbols);
+
+  const recentFinancials = formatIncomeStatementsToRecentFinancials(stocks, incomeStatements);
+  recentFinancialsModel.saveAll(recentFinancials);
+
+  const stockPrices = await stockPriceModel.getAll(stocks);
+  await stockPriceModel.saveAll(stockPrices);
+};
+
+main();
