cf0ba41ee9e7c1d1ea6015253f584f2f4048c3dc
Serge Vartanov <vartanov.s@gmail.com>
2021-06-12 23:52:31 -0500

    add configs for crypto query

--- src/constants/flags.ts
+++ src/constants/flags.ts
@@ -1,7 +1,8 @@
 // Control flags
-
+export const UPDATE_CRYPTO = false;
 export const UPDATE_INCOME_STATEMENTS = false;
 export const UPDATE_HISTORIC_STOCK_PRICES = false;
 export const UPDATE_STOCK_LIST = false;
 export const UPDATE_STOCK_PRICES = false;
 export const UPSERT_INCOME_STATEMENTS = false;
+ 
