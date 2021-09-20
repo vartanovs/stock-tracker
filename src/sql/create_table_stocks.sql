83ad930c2be714e8438751aec35563a5212b9069
Serge Vartanov <vartanov.s@gmail.com>
2021-09-19 19:38:22 -0500

    update create table sql statement with industry and sector columns

--- src/sql/create_table_stocks.sql
+++ src/sql/create_table_stocks.sql
@@ -1,5 +1,7 @@
 CREATE TABLE stocks (
     symbol VARCHAR(6) PRIMARY KEY,
     exchange_type VARCHAR(6),
-    name VARCHAR(36)
+    name VARCHAR(64),
+    sector VARCHAR(64),
+    industry VARCHAR(64)
 );
