4c15a73077ee4a8add8c05a7ddefe9d0bb486010
Serge Vartanov <vartanov.s@gmail.com>
2021-12-28 15:22:22 -0600

    update stocks sql table with central_index_key

--- src/sql/create_table_stocks.sql
+++ src/sql/create_table_stocks.sql
@@ -2,6 +2,7 @@ CREATE TABLE stocks (
     symbol VARCHAR(6) PRIMARY KEY,
     exchange_type VARCHAR(6),
     name VARCHAR(64),
+    central_index_key VARCHAR(10),
     sector VARCHAR(64),
     industry VARCHAR(64)
 );
